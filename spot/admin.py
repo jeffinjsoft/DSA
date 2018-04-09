769a0ef272e02ee9ffd9e7ebb4fd1d5a87920db3
Jeff <jeffin.jsoft@gmail.com>
2018-04-10 00:26:56 +0530

    action added

--- spot/admin.py
+++ spot/admin.py
@@ -9,22 +9,16 @@ from django.contrib import admin
 from . import models
 
 
-def start(modeladmin, request, queryset):
-    queryset.update(active=True)
-    for i in queryset:
-        print i.key
-start.short_description = "Start"
 
-def stop(modeladmin, request, queryset):
-    queryset.update(active=False)
-    for i in queryset:
-        print i.key
-stop.short_description = "Stop"
+
+
 
 @admin.register(models.Asg)
 class AsgAdmin(admin.ModelAdmin):
+    # items display in main
     list_display = ('name', 'active','instance_type','refreshed_at')
 
+    # field set in add option
     fieldsets = (
         ('Main', {
             'fields': ('name', 'vpc', 'sgs')
@@ -38,5 +32,29 @@ class AsgAdmin(admin.ModelAdmin):
         }),
     )
 
+
+    ## additional admin actions
+
+    def start(self, request, queryset):
+         rows = queryset.update(active=True)
+         if rows == 1:
+                message_bit = "1 story was"
+         else:
+            message_bit = "%s stories were" % rows_updated
+         self.message_user(request, "%s successfully marked as published." % message_bit)
+
+    start.short_description = "Start"
+
+
+    def stop(self, request, queryset):
+        rows = queryset.update(active=False)
+        if rows == 1:
+               message_bit = "1 story was"
+        else:
+           message_bit = "%s stories were" % rows_updated
+        self.message_user(request, "%s successfully marked as published." % message_bit)
+    stop.short_description = "Stop"
+
+    # admin action register
     actions = [start,stop]
     # fields = ['name', 'ami']
